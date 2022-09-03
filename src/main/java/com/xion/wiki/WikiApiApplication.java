d6f7f4e2cf15d4c1144227e98dcbe8c24e9ef4f0
xiye <Zhou730428@outlook.com>
2022-09-03 13:32:19 +0800

    first commit

---
+++ src/main/java/com/xion/wiki/WikiApiApplication.java
@@ -0,0 +1,13 @@
+package com.xion.wiki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WikiApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WikiApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/xion/wiki/WikiApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xion.wiki;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WikiApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
