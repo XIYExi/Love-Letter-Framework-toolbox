70a3f95b604762d73986c87f18a8cde896651d6d
xiye <Zhou730428@outlook.com>
2022-09-10 19:05:03 +0800

    (补)提交content内容

---
+++ src/main/java/com/xion/wiki/domain/Content.java
@@ -0,0 +1,21 @@
+package com.xion.wiki.domain;
+
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class Content {
+
+    @JsonSerialize(using = ToStringSerializer.class)
+    private Long id;
+
+    private String content;
+
+}
